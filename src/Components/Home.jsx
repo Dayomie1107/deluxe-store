a874c5e8c91d4591d453897e9fe90963a4b2a432
Daniel Ayomide <danielayomide356@gmail.com>
2026-09-23 14:22:50 +0100

    fix categories json filename

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../home/Navbar'
 import Hero from '../home/Hero'
-import Categories from '../home/Categories'
+import categories from '../home/categories'
 import WhyChooseUs from '../home/WhyChooseUs'
 import FeaturedCollection from '../home/FeaturedCollection'
 import Newsletter from './Newsletter'
@@ -11,7 +11,7 @@ function Home({ addToCart }) {
   return (
     <div>
       <Hero />
-      <Categories />
+      <categories />
       <WhyChooseUs />
       <FeaturedCollection addToCart={addToCart} />
       <Newsletter />
